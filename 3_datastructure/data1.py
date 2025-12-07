2c98c5cf69de943aa76eeb52c4e85d9d0f0c82b5
sunilsala88 <sunilsala88@gmail.com>
2025-12-07 18:31:28 +0530

    first commit

---
+++ 3_datastructure/data1.py
@@ -0,0 +1,20 @@
+
+# 4 data type int,float,bool,str
+#4 more data type today
+#data structure (collection of data)
+#datastructure ,datatype 
+#list,dictionary,tuple,set
+
+l1=[10,20,30,40,50,'jkl'] #list
+print(type(l1))
+print(l1)
+
+#indexing
+print(l1[0],l1[-1])
+
+#slicing
+print(l1[0:2])
+
+#update
+l1[-1]=60
+print(l1)
