30c8c2532c352bca03830f80d4709c3ce3a5778f
sunilsala88 <sunilsala88@gmail.com>
2025-12-08 12:23:12 +0530

    dec-8

--- 3_datastructure/data2.py
+++ 3_datastructure/data2.py
@@ -5,9 +5,12 @@ stock_prices={'tsla':400,'goog':900,'reliance':500,'nifty':1000}
 #access
 print(stock_prices['tsla'])
 
-print(stock_prices.get('tsla'))
+print(stock_prices.get('tsa',0))
 
 #update
+
+stock_prices['goog']=930
+
 stock_prices.update({'goog':930})
 print(stock_prices)
 
@@ -19,5 +22,18 @@ print(stock_prices)
 stock_prices.pop('nifty')
 print(stock_prices)
 
+# del stock_prices['nifty']
 
 #keys,values,items
+
+#sets
+s1={3,4,5,5}
+s2=set([3,4,5,4])
+
+print(s1)
+print(s2)
+
+print(s1.add(70))
+print(s1)
+
+#list,str,dictionary
