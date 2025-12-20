44198f9d51b0830457f8e9f7ede2c5d130418e56
sunilsala88 <sunilsala88@gmail.com>
2025-12-20 18:35:18 +0530

    dec-20-1

---
+++ 5_loops/none1.py
@@ -0,0 +1,24 @@
+
+
+l1=[11,22,33,44]
+l2=[11,33,22,44]
+l3=[11,22,33,44]
+print(l1==l2)
+print(l1==l3)
+
+d1={'a':11,'b':22,'c':33}
+d2={'a':11,'c':33,'b':22}
+print(d1==d2)
+
+if None:
+    print('inside if')
+else:
+    print('inside else')
+
+a=None
+print(type(a))
+print(type(l1))
+
+
+#falsy value
+#0,False,'',[],{},None
