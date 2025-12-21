7cf3aa3cf7a426961b903928136c0acf24d4e2e8
sunilsala88 <sunilsala88@gmail.com>
2025-12-21 19:11:54 +0530

    dec-21-5

--- 5_loops/while1.py
+++ 5_loops/while1.py
@@ -2,7 +2,27 @@
 num=0
 
 while True:
-    if num==10:
+    if num==5:
         break
     print(num)
+
     num=num+1
+
+
+# num=0
+# while num!=10:
+#     print(num)
+#     num=num+1
+
+
+# num=0
+# even=[]
+# while True:
+#     if num==101:
+#         break
+#     print(num)
+#     if num%2==0:
+#         even.append(num)
+
+#     num=num+1
+# print(even)
