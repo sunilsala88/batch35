31df5b91f7d2cbc4a10f95440c2caeded898325c
sunilsala88 <sunilsala88@gmail.com>
2025-12-27 18:52:07 +0530

    dec-27-2

--- 5_loops/problems.py
+++ 5_loops/problems.py
@@ -2,7 +2,7 @@
 
 
 l2=[22,33,44,55]
-s2='tsla'
+
 
 final=[]
 for i in l2:
@@ -20,3 +20,22 @@ final3=[]
 for i in range(-1,-(len(l2)+1),-1):
     final3.append(l2[i])
 print(final3)
+
+
+s2='tsla'
+
+f1=''
+for i in s2:
+    f1=i+f1
+print(f1)
+
+f2=''
+#positive index
+for i in range(len(s2)-1,-1,-1):
+    f2=f2+s2[i]
+print(f2)
+
+f3=''
+for i in range(-1,-(len(s2)+1),-1):
+    f3=f3+s2[i]
+print(f3)
