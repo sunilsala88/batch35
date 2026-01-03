4dde5dfbc960abeb5d612ff42d78f9c5264500f3
sunilsala88 <sunilsala88@gmail.com>
2026-01-03 18:40:21 +0530

    jan3-2

--- 7_files_exception/files1.py
+++ 7_files_exception/files1.py
@@ -1,7 +1,12 @@
 
 
-data='goog=200'
+# data='goog=200'
 
-f1=open('prices.txt','w')
-f1.write(data)
-f1.close()
+# f1=open('prices.txt','w')
+# f1.write(data)
+# f1.close()
+
+f2=open(r'/Users/algo trading 2026/batch35/7_files_exception/data2.txt','r')
+d=f2.read()
+print(d)
+f2.close()
