b7e2fd145db522e19efc354b76dad9f05e2ac2ef
sunilsala88 <sunilsala88@gmail.com>
2026-01-10 19:29:28 +0530

    jan-10-44

--- 9_module/module1.py
+++ 9_module/module1.py
@@ -3,10 +3,17 @@
 #module/library/package
 
 import file2
+import file2 as f1
+import demo.sample1 as t1
+import alskdjflkajsdflkajdflkajdlfk as a1
+print(a1.a)
 
 print(file2.name)
+print(f1.name)
 print(file2.price)
 print(file2.get_current_price())
 
 c1=file2.Circle(10)
 print(c1)
+
+print(t1.free)
