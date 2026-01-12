3366b04083c58b4fc3e5b384540567ff8d6dc73c
sunilsala88 <sunilsala88@gmail.com>
2026-01-12 20:20:36 +0530

    jan-12

--- 10_virutal_env/install1.py
+++ 10_virutal_env/install1.py
@@ -7,3 +7,15 @@ import selenium
 
 #pip3 list
 #pip3 install library_name==version
+
+import datetime
+import logging
+
+import pandas
+import numpy
+
+#alpaca alpaca python
+#interactive ibinsync
+
+#yfinance
+#mplfinance plot
