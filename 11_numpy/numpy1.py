2b34be9b6088e99298f3d46618f3d23cd0c6402c
sunilsala88 <sunilsala88@gmail.com>
2026-01-17 18:53:35 +0530

    jan-17-3

--- 11_numpy/numpy1.py
+++ 11_numpy/numpy1.py
@@ -1,9 +1,25 @@
 
 import numpy as np
-
+#numpy array
 l1=[11,22,33,'55']
 print(l1)
-np1=np.array(l1)
+np1=np.array(l1,dtype='int')
 print(np1)
 
-#numpy array
+l2=[1,2,3,4]
+np2=np.array(l2,dtype=int)
+print(np2)
+
+# print(l1-l2)
+print(np1%np2)
+
+# print(l1+3)
+print(np1*7)
+
+print(np.ones(10,dtype=int))
+print(np.zeros(5,dtype=int))
+print(np.arange(10,30,2))
+print(np.random.randint(100,200,10))
+
+a=np.arange(9)
+print(a)
---
+++ 11_numpy/numpy2.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+
+np1=np.arange(9).reshape(3,3)
+np2=np.random.randint(100,200,25).reshape(5,5)
+print(np1)
+print(np2)
