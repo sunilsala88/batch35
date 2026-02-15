0368dc41f531302099451648992f213bc766b6ac
sunilsala88 <sunilsala88@gmail.com>
2026-02-16 00:32:51 +0530

    feb-15-last

--- 16_fin_data_analysis/plot1.py
+++ 16_fin_data_analysis/plot1.py
@@ -8,6 +8,9 @@ s=e-dt.timedelta(days=3)
 print(s,e)
 data=yf.download('TSLA',start=s,end=e,multi_level_index=False,interval='1m',ignore_tz=True)
 
+
+data['sma']=data['Close'].rolling(window=5).mean()
 print(data)
 import mplfinance as mpf
-mpf.plot(data,type='candle',style='yahoo')
+p=mpf.make_addplot(data['sma'],color='blue')
+mpf.plot(data,type='candle',style='yahoo',addplot=p)
