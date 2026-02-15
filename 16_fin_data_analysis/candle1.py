c44dea7dc3e005532e86264140045fea6dbe5dc5
sunilsala88 <sunilsala88@gmail.com>
2026-02-15 18:32:10 +0530

    feb15-1

--- 16_fin_data_analysis/candle1.py
+++ 16_fin_data_analysis/candle1.py
@@ -6,5 +6,5 @@ import datetime as dt
 e=dt.datetime.now()
 s=e-dt.timedelta(days=5)
 print(s,e)
-data=yf.download('TSLA',start=s,end=e,multi_level_index=False,interval='1m',ignore_tz=True)
+data=yf.download('TSLA',start=s,end=e,multi_level_index=False,interval='1m')
 print(data)
