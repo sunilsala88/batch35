cdbd7cd39c30617b7546114e0267bfe163ca3271
sunilsala88 <sunilsala88@gmail.com>
2026-02-28 19:24:50 +0530

    feb-28-44

--- 18_ibk_broker_api/basic.py
+++ 18_ibk_broker_api/basic.py
@@ -12,20 +12,35 @@
 
 
 
-from ib_async import *
-# util.startLoop()  # uncomment this line when in a notebook
+# from ib_async import *
+# # util.startLoop()  # uncomment this line when in a notebook
 
-ib = IB()
-ib.connect('127.0.0.1', 7497, clientId=1)
+# ib = IB()
+# ib.connect('127.0.0.1', 7497, clientId=1)
 
-# Request historical data
-contract = Forex('EURUSD')
-bars = ib.reqHistoricalData(
-    contract, endDateTime='', durationStr='30 D',
-    barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+# # Request historical data
+# contract = Forex('EURUSD')
+# bars = ib.reqHistoricalData(
+#     contract, endDateTime='', durationStr='30 D',
+#     barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
 
-# Convert to pandas dataframe (pandas needs to be installed):
-df = util.df(bars)
-print(df.head())
+# # Convert to pandas dataframe (pandas needs to be installed):
+# df = util.df(bars)
+# print(df.head())
 
-ib.disconnect()
+# ib.disconnect()
+
+
+# from ib_async import *
+
+# ib = IB()
+# ib.connect('127.0.0.1', 7497, clientId=1)
+
+# # Get account summary
+# account = ib.managedAccounts()[0]
+# summary = ib.accountSummary(account)
+# for item in summary:
+#     print(f"{item.tag}: {item.value}")
+
+# ib.disconnect()
+
