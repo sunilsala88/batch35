c9115aa56cdd7570cb5c07a1f0e6c1b114532bfd
sunilsala88 <sunilsala88@gmail.com>
2026-04-19 18:25:57 +0530

    apr-19-1

--- 21_strategy_template/telegram1.py
+++ 21_strategy_template/telegram1.py
@@ -4,8 +4,8 @@
 
 
 import requests
-TOKEN = '8120146889:AAF8we4pr6rhiMGJcUFidpI4l-fM0Khnc68'
-ids = '5563890177'
+TOKEN = ''
+ids = ''
 
 message='second message'
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ids}&parse_mode=Markdown&text={message}"
